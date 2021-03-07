0fd8f96fc94c298b472263cf1cf1266117d861ae
MIT leon <leonc@mit.edu>
2021-03-07 18:00:53 -0500

    quick homepage design

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import styled from 'styled-components';
 import { Homepage } from './homepage/_homepage';
 
 const AppContainer = styled.div`
-  * {
-    box-sizing: border-box;
-  }
+  margin: 0;
 `;
 
 export const App = (props: {}) => {
--- src/homepage/_homepage.tsx
+++ src/homepage/_homepage.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HomepageContainer = styled.div`
-    
+    border: 100px solid #f7a18b;
+    background-color: #111147;
+    margin: 0;
+
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    p {
+        color: white;
+        font-size: 5vh;
+        vertical-align: center;
+        text-align: center;
+    }
 `;
 
 export const Homepage = (props: {}) => {
     return (
         <HomepageContainer>
-            Hi there this is the homepage
+            <p>
+                MIT UIUX Club Website 
+            </p>
+            <p>
+                ~~ Coming soon in a few weeks! ~~
+            </p>
         </HomepageContainer>
     )
 }
